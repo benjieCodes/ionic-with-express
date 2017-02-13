89fedf6cbae815febf47b14ab706a3883e48249b
benjiecodes <benjiecodes@gmail.com>
2017-02-13 15:39:59 -0500

    CRUD todo is complete

--- api/app.js
+++ api/app.js
@@ -32,7 +32,7 @@ app.post('/todos', function (req, res) {
   })
 })
 app.put('/todos/:id', function (req, res) {
-  var sql = `UPDATE todos SET title ='${req.body.title}', completed ='${req.body.completed}' WHERE id = '${req.body.id}'`;
+  var sql = `UPDATE todos SET title ='${req.body.title}', completed ='${req.body.completed} 'WHERE id ='${req.body.id}'`;
   database.query({sql:sql}).then(function (results) {
     res.send(results)
   }).catch(function (err) {
@@ -41,7 +41,7 @@ app.put('/todos/:id', function (req, res) {
   })
 })
 app.delete('/todos/:id', function (req, res) {
-  var sql = `DELETE FROM todos WHERE id ='${req.body.id}'` ;
+  var sql = `DELETE FROM todos WHERE id ='${req.params.id}'` ;
   database.query({sql:sql}).then(function (results) {
     res.send(results)
   }).catch(function (err) {
